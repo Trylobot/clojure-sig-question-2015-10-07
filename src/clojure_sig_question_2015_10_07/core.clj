9ac2a4e4c66a53cfc075125720326a406407cf61
Tyler Cole <tyler.cole@yodle.com>
2015-10-07 15:46:16 -0500

    answered own question

--- src/clojure_sig_question_2015_10_07/core.clj
+++ src/clojure_sig_question_2015_10_07/core.clj
@@ -1,8 +1,21 @@
 (ns clojure-sig-question-2015-10-07.core)
 
-(defn add-item [collection item stack-key]
+; working (original) version
+(defn add-item-1 [collection item stack-key]
   (let [
     current-stack (collection stack-key)]
   (merge collection {stack-key (if current-stack
     (conj current-stack item)
     [item])})) )
+
+; experimental version using update-in
+(defn add-item-upd [collection item stack-key]
+  (update-in collection [stack-key]
+    (fn [current-stack] (if current-stack
+      (conj current-stack item)
+      [item]))) )
+
+; current binding (under test)
+;(def add-item add-item-1)
+(def add-item add-item-upd)
+
